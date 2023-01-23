0221f2f949571123518c5a50cd394a5c70d64187
Lila Rest <mail@lila.rest>
2023-01-23 12:37:20 +0100

    fix: Allow custom classes to be properly added even if the target element has been modified

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+import { read } from 'fs';
 import { hasPseudoElements, parseRulesets, parseSelector, splitPseudoElements } from 'not-a-real-css-parser';
 import { Plugin } from 'obsidian';
 
@@ -27,30 +28,50 @@ export default class extends Plugin {
         });
 
         // Render the custom block classes
-        let nextBlockClass: string | null = null;
-        this.registerMarkdownPostProcessor((element) => {
+        this.registerMarkdownPostProcessor(
+            (element, context) => {
 
-            // If the previous element was a custom class block
-            if (nextBlockClass) {
-
-                // Add the custom class to the current element
-                element.classList.add(nextBlockClass);
-
-                // Reset the nextBlockClass and return
-                nextBlockClass = null;
-                return;
-            }
-
-            // Else if the current block is a custom class block
-            const classBlock = [...element.querySelectorAll('code')].find((codeEl) => codeEl.innerText.trim().startsWith(CLASSNAME_TAG));
-            if (classBlock) {
-
-                // Retrieve the custom class name for the next block
-                nextBlockClass = classBlock.innerText.trim().replace(CLASSNAME_TAG, "").trim();
+                // Retrieve the blocks' container element
+                const blocksContainer = context.containerEl;
                 
-                // Remove the classBlock element from the render
-                classBlock.parentElement?.remove()
+                // Listen for insertion of the element into the blocks' container
+                const observer = new MutationObserver(() => {
+
+                    // If the element has been inserted
+                    if (blocksContainer.contains(element)) {
+
+                        // And if the current block is a custom class block
+                        const classBlock = [...element.querySelectorAll('code')].find((codeEl) => codeEl.innerText.trim().startsWith(CLASSNAME_TAG));
+                        if (classBlock) {
+
+                            // Retrieve the custom class name
+                            const nextBlockClass = classBlock.innerText.trim().replace(CLASSNAME_TAG, "").trim();
+
+                            // Set the 'data-next-block-class' attribute with the custom class name
+                            element.setAttribute("data-next-block-class", nextBlockClass);
+                            
+                            // Remove the classBlock element from the render
+                            element.innerHTML = "";
+                        }
+
+                        // Or if the current block is not a custom class block
+                        else {
+
+                            // If it is preceded by a custom class block
+                            const previousElement = blocksContainer.children.item([...blocksContainer.children].indexOf(element) - 1)
+                            if (previousElement && previousElement.getAttribute("data-next-block-class")) {
+                                
+                                // Set the custom class to the current element
+                                element.classList.add(previousElement.getAttribute("data-next-block-class"))
+                            }
+                        }
+
+                        // Finally, stop listening for element insertion.
+                        observer.disconnect();
+                    }
+                });
+                observer.observe(blocksContainer, { attributes: false, childList: true, characterData: false, subtree: true });
             }
-        });
+        );
     }
 }
